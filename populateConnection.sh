3fe08f6e89347c56688c5ef02fe4afe61be86993
magnusmile <magnus.leung@mile.cloud>
2025-03-07 11:30:52 +0800

    restructure

--- populateConnection.sh
+++ populateConnection.sh
@@ -2,11 +2,11 @@
 # Script used in github actions to run test the schemachange functionality against the demo scenarios included in the repository.
 touch ./connections.toml
 echo [default] >> ./connections.toml
-echo account = \"${SNOWFLAKE_ACCOUNT}\" >> ./connections.toml
-echo user = \"${SNOWFLAKE_USER}\" >> ./connections.toml
-echo role = \"${SNOWFLAKE_ROLE}\" >> ./connections.toml
-echo warehouse = \"${SNOWFLAKE_WAREHOUSE}\" >> ./connections.toml
-echo database = \"${SNOWFLAKE_DATABASE}\" >> ./connections.toml
-echo password = \"${SNOWFLAKE_PASSWORD}\" >> ./connections.toml
+echo account = \"${SF_ACCOUNT}\" >> ./connections.toml
+echo user = \"${SF_USERNAME}\" >> ./connections.toml
+echo role = \"${SF_ROLE}\" >> ./connections.toml
+echo warehouse = \"${SF_WAREHOUSE}\" >> ./connections.toml
+echo database = \"${SF_DATABASE}\" >> ./connections.toml
+echo password = \"${SF_PASSWORD}\" >> ./connections.toml
 echo "cat connections.toml"
 cat ./connections.toml
